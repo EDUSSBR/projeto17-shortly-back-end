419a803fc3e6717f8ac485427523af0f995438af
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-17 12:36:18 -0300

    refactor: change links router for controller isolation

--- src/routers/links.route.js
+++ src/routers/links.route.js
@@ -1,29 +1,13 @@
 import { Router } from "express";
+import { createShortenLink, deleteLink, getLinkByID, getLinksRank, redirectToLink } from "../controllers/links.controller.js";
 
 
 const linksRoute = Router();
 
-//auth
-linksRoute.post("/urls/shorten", (req, res) => {
-    res.send("urls/shorten")
-
-})
-
-//no
-linksRoute.get("/urls/:id", (req, res) => {
-    res.send("/urls/:id")
-})
-
-linksRoute.get("/urls/open/:shortUrl", (req, res) => {
-    res.send("/urls/open/:shortUrl")
-})
-
-//auth
-linksRoute.delete("/urls/:id", (req, res) => {
-    res.send("/urls/:id")
-})
-linksRoute.get("/ranking", (req, res) => {
-    res.send("/ranking")
-})
+linksRoute.post("/urls/shorten", createShortenLink)
+linksRoute.get("/urls/:id", getLinkByID)
+linksRoute.get("/urls/open/:shortUrl", redirectToLink)
+linksRoute.delete("/urls/:id",deleteLink)
+linksRoute.get("/ranking", getLinksRank)
 
 export { linksRoute }
