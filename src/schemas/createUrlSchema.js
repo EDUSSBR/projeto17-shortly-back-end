22f22eddce2a29d944061ac5358735965a680f35
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-22 08:53:19 -0300

    fix: change url validation for fitting front-end with the same

--- src/schemas/createUrlSchema.js
+++ src/schemas/createUrlSchema.js
@@ -1,7 +1,9 @@
 import Joi from 'joi';
 
 const urlSchema = Joi.object({
-	url: Joi.string().uri().required()
+	url: Joi.string()
+    .regex(/^(?:https?:\/\/)?(w{3}\.)?[\w_-]+((\.\w{2,}){1,2})(\/([\w\._-]+\/?)*(\?[\w_-]+=[^\?\/&]*(\&[\w_-]+=[^\?\/&]*)*)?)?$/)
+    .required()
 });
 
 export { urlSchema };
